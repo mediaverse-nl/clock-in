ac83cdc18379ebb3bfc4e231f3db6091cc34858e
deveron reniers <deveron-reniers@hotmail.com>
2018-10-27 18:13:44 +0200

    database upgrade

---
+++ database/migrations/2018_10_27_171023_create_package_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreatePackageTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('package', function (Blueprint $table) {
+            $table->increments('id');
+            $table->unsignedInteger('business_id')->unsigned();
+            $table->foreign('business_id')->references('id')->on('business');
+            $table->string('staff_members');
+            $table->string('devices');
+            $table->string('price');
+            $table->string('subscription');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('package');
+    }
+}
